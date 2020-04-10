32b976a230bc2abe5c5da9f612aec72532fcd77b
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-04-10 12:52:41 +0200

    Avoid installing unused `cargo-tarpaulin` in CI

--- ci/travis/setup.sh
+++ ci/travis/setup.sh
@@ -2,19 +2,6 @@
 
 . $(dirname $0)/functions.sh
 
-# --- Setup cargo-tarpaulin ----------------------------------------------------------
-
-LATEST=$(cargo search cargo-tarpaulin | grep cargo-tarpaulin | cut -f2 -d"\"")
-LOCAL=$(cargo tarpaulin --version 2>/dev/null | cut -d" " -f3 || echo "none")
-
-if [ "$LATEST" != "$LOCAL" ]; then
-  log Installing cargo-tarpaulin v$LATEST
-  cargo install -f cargo-tarpaulin --root "$HOME/.cargo"
-else
-  log Using cached cargo-tarpaulin v$LOCAL
-fi
-
-
 # --- Setup cargo-cache ------------------------------------------------------
 
 LATEST=$(cargo search cargo-cache | head -n1 | cut -f2 -d"\"")
